4e971e327211388871e8cdb1691a0c0c615d0e1c
adeleong <aderso.deleon@gmail.com>
2016-07-01 17:16:41 -0400

    Posting data, Page Navigation Part 1- 8:11

---
+++ AppWebForm/AppWebForm/Models/ComputerOrder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppWebForm.Models
+{
+    public class ComputerOrder
+    {
+        public int OrdenId { get; set; }
+        public string OrdenName { get; set; }
+        public string Customer { get; set; }
+        public string CustomerEmail { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public int PartNumber  { get; set; }
+        public bool Rush { get; set; }
+
+    }
+}
